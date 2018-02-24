6a7f33084996da7bba907d2d6ea0c1773487db23
Ancy <lazuliroseancy@gmail.com>
2018-02-24 12:45:41 +0900

    Add images Fix link,CSS

--- partner.php
+++ partner.php
@@ -27,6 +27,11 @@ Template Name: PARTNER
                             <li class="banner">
                                 <a class="effect" href="http://www.fudai-kouenkai.osakafu-u.ac.jp/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/fudai-kouenkai.png" alt="大阪府立大学 後援会" itemprop="image"><h3>大阪府立大学後援会</h3></a>
                             </li>
+                            <li class="banner">
+                                <a class="effect" href="http://www.osaka-jc.or.jp/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/jci.png" alt="JCI" itemprop="image">
+                                    <h3>一般社団法人 大阪青年会議所</h3>
+                                </a>
+                            </li>
                         </ul>
                     </div>
                     <div class="l-inkind" itemscope itemtype="http://schema.org/makesOffer">
@@ -41,15 +46,13 @@ Template Name: PARTNER
                                 <a class="effect" href="http://www.senoya.jp/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/senoya.png" alt="せのや" itemprop="image"><h3>株式会社せのや</h3></a>
                             </li>
                             <li class="banner">
-                                <a class="effect" href="http://www.cima-net.co.jp/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/chibo.png" alt="千房" itemprop="image">
+                                <a class="effect" href="http://www.chibo.com/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/chibo.png" alt="千房" itemprop="image">
                                 <h3>千房</h3>
                                 </a>
                             </li>
-                            <!--
                             <li class="banner">
-                                <a class="effect" href="" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/" alt="デジタルハリウッド" itemprop="image"></a>
+                                <a class="effect" href="http://www.dhw.co.jp/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/dhw.png" alt="デジタルハリウッド" itemprop="image"><h3>デジタルハリウッド</h3></a>
                             </li>
--->
                             <li class="banner">
                                 <a class="effect" href="http://www.fujixerox.co.jp/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/fujixerox.png" alt=".png 富士ゼロックス" itemprop="image">
                                     <h3>富士ゼロックス株式会社</h3>
@@ -60,7 +63,36 @@ Template Name: PARTNER
                                     <h3>Peatix</h3>
                                 </a>
                             </li>
-
+                            <li class="banner">
+                                <a class="effect" href="http://www.kushikatu-daruma.com/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/daruma.png" alt="串かつだるま" itemprop="image">
+                                    <h3>串かつだるま</h3>
+                                </a>
+                            </li>
+                            <li class="banner">
+                                <a class="effect" href="http://brooklynroasting.jp/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/brooklyn.png" alt="Brooklyn Roasting Company" itemprop="image">
+                                    <h3>Brooklyn Roasting Company</h3>
+                                </a>
+                            </li>
+                            <li class="banner">
+                                <a class="effect" href="https://www.brushup.net/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/brushup.png" alt="デザインレビューツール Brushup" itemprop="image">
+                                    <h3>デザインレビューツール Brushup</h3>
+                                </a>
+                            </li>
+                            <li class="banner">
+                                <a class="effect" href="http://www.raple.co.jp/" target="_blank" itemprop="url"><img src="<?php bloginfo('template_directory'); ?>/images/raple.png" alt="株式会社ラプレ" itemprop="image">
+                                    <h3>株式会社ラプレ</h3>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="l-logo" itemscope itemtype="http://schema.org/makesOffer">
+                        <h2 class="inkind-header effect">Logo Partner</h2>
+                        <ul class="list-banner">
+                            <li class="banner">
+                                <!--<a href="#" target="_blank" itemprop="url">--><img class="effect" src="<?php bloginfo('template_directory'); ?>/images/azconnect.png" alt="株式会社アズコネクト" itemprop="image">
+                                <h3>株式会社アズコネクト</h3>
+                                <!--                                </a>-->
+                            </li>
                         </ul>
                     </div>
 
